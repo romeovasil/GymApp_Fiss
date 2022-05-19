8ba281248bf66e294c88b1c6ce2813f672f9680e
romeo vasil <romeo.vasil@icloud.com>
2022-05-19 10:35:24 +0100

    The manager can now see a message , the req is automaticaly deleted after response

--- src/main/java/org/loose/fis/sre/controllers/ManagerViewReqController.java
+++ src/main/java/org/loose/fis/sre/controllers/ManagerViewReqController.java
@@ -2,6 +2,8 @@ package org.loose.fis.sre.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.scene.text.Text;
+import org.loose.fis.sre.exceptions.NoReqSelected;
 import org.loose.fis.sre.model.Classes;
 import org.loose.fis.sre.model.Requests;
 import org.loose.fis.sre.services.ClassesService;
@@ -9,8 +11,11 @@ import org.loose.fis.sre.services.ReqService;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class ManagerViewReqController {
+    @FXML
+    private Text registrationMessage;
 
     @FXML
     private ListView<String> req = new ListView<>() ;
@@ -20,29 +25,61 @@ public class ManagerViewReqController {
     private List <String> listaString = new ArrayList<>();
 
     private String selectedReq;
+    private int selectedIndex;
+
+    public void setSelectedReqNull(){
+        this.selectedReq= "";
+    }
 
     public void refreshReq() {
 
         listReq=ReqService.getReqList();
         for (Requests requests : listReq) {
-            listaString.add(requests.toString());
+            if(!(Objects.equals(requests.getStatus(),"  - ACCEPTED")||Objects.equals(requests.getStatus(),"  - REJECTED")))
+                listaString.add(requests.toString());
         }
         req.getItems().addAll(listaString);
 
     }
+    private void checkSelectedReq() throws NoReqSelected{
+        if (Objects.equals(this.selectedReq,""))
+        {
+            throw new NoReqSelected();
+        }
+    }
 
     public void acceptReq(){
-        ReqService.addStatus(this.selectedReq,"  - ACCEPTED");
+        try {
+            checkSelectedReq();
+            ReqService.addStatus(this.selectedReq, "  - ACCEPTED");
+            req.getItems().remove(this.selectedIndex);
+            registrationMessage.setText("Req successfully accepted");
+            this.selectedReq="";
+        }catch (NoReqSelected e)
+        {
+            registrationMessage.setText(e.getMessage());
+        }
 
 
     }
 
     public void declineReq(){
-        ReqService.addStatus(this.selectedReq,"  - REJECTED");
+        try {
+            checkSelectedReq();
+
+            ReqService.addStatus(this.selectedReq, "  - REJECTED");
+            req.getItems().remove(this.selectedIndex);
+            registrationMessage.setText("Req successfully declined");
+            this.selectedReq="";
+        }catch (NoReqSelected e)
+        {
+            registrationMessage.setText(e.getMessage());
+        }
     }
 
     public void selectedReq(){
         this.selectedReq=req.getSelectionModel().getSelectedItem();
+        this.selectedIndex = req.getSelectionModel().getSelectedIndex();
     }
 
 
---
+++ src/main/java/org/loose/fis/sre/exceptions/NoReqSelected.java
@@ -0,0 +1,13 @@
+package org.loose.fis.sre.exceptions;
+
+public class NoReqSelected  extends Exception {
+
+
+
+    public  NoReqSelected() {
+        super( "No req selected");
+
+    }
+
+
+}
