9b897429d55fdc2c4b964a74b977facbb6eeab83
Tripa Eric <eric_tripa@yahoo.com>
2022-05-21 19:26:34 +0300

    Added design of the Card Payment option

---
+++ src/main/java/org/loose/fis/sre/controllers/CardPaymentController.java
@@ -0,0 +1,17 @@
+package org.loose.fis.sre.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class CardPaymentController {
+    public Label labelMembership;
+    public Text successMessage;
+    public TextField cvvField;
+    public TextField nameField;
+    public TextField numberField;
+
+    public void handleAddCardAction(ActionEvent actionEvent) {
+    }
+}
