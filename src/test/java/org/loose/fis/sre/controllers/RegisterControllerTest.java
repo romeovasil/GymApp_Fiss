faea192da6334b73fa0bd5577d05c11feba81e4c
Romeo <44144366+romeovasil@users.noreply.github.com>
2022-05-22 12:13:31 +0300

    RegisterControllerTest added

--- src/test/java/org/loose/fis/sre/controllers/RegisterControllerTest.java
+++ src/test/java/org/loose/fis/sre/controllers/RegisterControllerTest.java
@@ -1,4 +1,68 @@
 package org.loose.fis.sre.controllers;
 
-public class RegisterControllerTest {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.stage.Stage;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.loose.fis.sre.services.FileSystemService;
+import org.loose.fis.sre.services.UserService;
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+
+import static org.testfx.assertions.api.Assertions.assertThat;
+
+@ExtendWith(ApplicationExtension.class)
+class RegisterControllerTest {
+
+    public static final String USERNAME = "user";
+    public static final String PASSWORD = "password";
+    public static final String OWNER = "Client";
+
+    @BeforeEach
+    void setUp() throws Exception {
+        FileSystemService.APPLICATION_FOLDER = ".registration-example";
+        FileUtils.cleanDirectory(FileSystemService.getApplicationHomeFolder().toFile());
+        UserService.initDatabase();
+    }
+
+    @Start
+    void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("register.fxml"));
+        primaryStage.setTitle("Registration Example");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+    @Test
+    void testRegistration(FxRobot robot) {
+        robot.clickOn("#usernameField");
+        robot.write(USERNAME);
+        robot.clickOn("#passwordField");
+        robot.write(PASSWORD);
+        robot.clickOn("#role");
+        robot.type(KeyCode.ENTER);
+        robot.clickOn("#registerButton");
+
+
+        assertThat(robot.lookup("#registrationMessage").queryText()).hasText("Account created successfully!");
+        assertThat(UserService.getAllUsers()).size().isEqualTo(1);
+
+        robot.clickOn("#registerButton");
+        assertThat(robot.lookup("#registrationMessage").queryText()).hasText(
+                String.format("An account with the username %s already exists!", USERNAME)
+        );
+
+        robot.clickOn("#usernameField");
+        robot.write("1");
+        robot.clickOn("#registerButton");
+
+        assertThat(robot.lookup("#registrationMessage").queryText()).hasText("Account created successfully!");
+        assertThat(UserService.getAllUsers()).size().isEqualTo(2);
+    }
 }
